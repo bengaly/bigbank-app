f5d399095422bc731b217c547760320635ad8870
bengaly <bk.konate@gmail.com>
2022-12-31 10:57:42 +0100

    finalized config management part

--- loans/src/main/java/com/bigbeng/loans/LoansApplication.java
+++ loans/src/main/java/com/bigbeng/loans/LoansApplication.java
@@ -3,11 +3,13 @@ package com.bigbeng.loans;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.ComponentScans;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@RefreshScope
 @ComponentScans({@ComponentScan("com.bigbeng.loans.controller")})
 @EnableJpaRepositories("com.bigbeng.loans.repository")
 @EntityScan("com.bigbeng.loans.model")
